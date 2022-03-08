56c72d5ef0b5d4680b6dad1e2c270024d1469a25
Yash Bhayani <83011938+Yashbhayani@users.noreply.github.com>
2022-03-08 15:09:55 +0530

    Update Page1.js

--- Userform1/Page1.js
+++ Userform1/Page1.js
@@ -1,10 +1,24 @@
 const ui = [];
+var Q =0;
+
+if(Q == 0){
+    let text = localStorage.getItem("User");
+    let obj = JSON.parse(text);
+
+    var HUI = "<thead>";
+        for(var i=0; i<obj.length;i++ ){
+            HUI += "<tr> <td>" + obj[i].FirstName + "</td> <td>" + obj[i].LastName +  "</td> <td>" + obj[i].Education_Filed + "</td> <td> " + obj[i].Gender + "</td><td> " + obj[i].Email + " </td> <td>" + obj[i].Password+ " </td></tr>";
+        }
+    HUI += "</thead>"
+    document.getElementById("UIO").innerHTML = HUI;
+    Q = 1;
+}
+
 function myfunction()
 {
     class user
     {
-        constructor(FirstName,LastName,Education_Filed,Email,Password)
-        {
+        constructor(FirstName,LastName,Education_Filed,Email,Password){
             this.FirstName = FirstName;
             this.LastName = LastName;
             this.EducationFiled = Education_Filed;
@@ -15,69 +29,63 @@ function myfunction()
  
     class user1 extends user
     {
-        constructor(FirstName,LastName,Education_Filed,Gender,Email,Password)
-        {
+        constructor(FirstName,LastName,Education_Filed,Gender,Email,Password){
             super(FirstName,LastName,Education_Filed,Gender,Email,Password)
             this.Gender = Gender; 
         }
 
         Usershow()
         {
-            ui.push(person);
             var Text = "<thread>";
-            var UI = ui.length;
+
+            let text = localStorage.getItem("User");
+            let obj = JSON.parse(text);
+            var UI = obj.length;
             var i;
 
             for(i=0; i<UI; i++){
-                Text +=  "<tr> <td>" + ui[i].FirstName + "</td> <td>" + ui[i].LastName + "</td> <td> " + ui[i].Education_Filed + "</td> <td> " + ui[i].Gender + " </td> <td>" + ui[i].Email + "</td> <td> " + ui[i].Password + " </td></tr>";
+                Text +=  "<tr> <td>" + obj[i].FirstName + "</td> <td>" + obj[i].LastName +  "</td> <td>" + obj[i].Education_Filed + "</td> <td> " + obj[i].Gender + "</td><td> " + obj[i].Email + " </td> <td>" + obj[i].Password+ " </td></tr>";
             }
             Text += "</thread>";
             document.getElementById('UIO').innerHTML = Text;
         }
     }
 
-
     var FirstName,LastName,Education_Filed,Gender,Email,Password;
     var person = {};
-
     person["FirstName"] = document.getElementById('exampleInputFname1').value;
     FirstName =  person["FirstName"];
-
     person["LastName"] = document.getElementById('exampleInputlname1').value;
-    LastName =  person["LastName"];
-    
+    LastName =  person["LastName"];    
     person["Education_Filed"] = document.getElementById('exampleInputlfiled').value;
     Education_Filed = person["Education_Filed"];
-    
-    
-    if(document.getElementById('inlineRadio1').checked){
+ 
+    if(document.getElementById('inlineRadio1').checked) {
         person["Gender"] = document.getElementById('inlineRadio1').value;
         Gender = person["Gender"];
-    } else if(document.getElementById('inlineRadio2').checked){
+    } else if(document.getElementById('inlineRadio2').checked) {
         person["Gender"] = document.getElementById('inlineRadio2').value;
         Gender = person["Gender"];
-    }else if(document.getElementById('inlineRadio3').checked){
+    } else if(document.getElementById('inlineRadio3').checked) {
         person["Gender"] = document.getElementById('inlineRadio3').value;
         Gender = person["Gender"];
     }
-  
+    
     person["Email"] = document.getElementById('exampleInputEmail1').value;
     Email = person["Email"];
-    
     person["Password"] = document.getElementById('exampleInputPassword1').value;
     Password = person["Password"];
-
-    if(FirstName === '' || LastName === '' || Education_Filed === '' || Gender === '' || Email === '' || Password === '') {
+    
+    if(FirstName === '' || LastName === '' || Education_Filed === '' || Gender === '' || Email === '' || Password === ''){
         alert("Some Property are Undefined!");
-    }else{
+    } else
+    {
         var User_recode = new Array();
         User_recode =JSON.parse(localStorage.getItem("User"))?JSON.parse(localStorage.getItem("User")):[]
-
-
-        if(User_recode.some((v) => {return v.Email == Email})){
+        
+        if(User_recode.some((v) => {return v.Email == Email})) {
             alert("Email id is a Already Existed!")
-        }else{
-    
+        } else   {
             User_recode.push({
                 FirstName : FirstName,
                 LastName : LastName,
@@ -90,14 +98,13 @@ function myfunction()
 
             const myUser = new user1(FirstName,LastName,Education_Filed,Gender,Email,Password);
             myUser.Usershow();
-            
-
 
     document.getElementById('exampleInputFname1').value = "";
     document.getElementById('exampleInputlname1').value = "";
     document.getElementById('exampleInputEmail1').value = "";
     document.getElementById('exampleInputPassword1').value = "";
-         
+         }
+
     }
 }
 
